c73399619bd4a9bf7de6499bcf538487f6bbcdac
haifenghuang <fenghai_hhf@163.com>
2019-09-17 11:17:28 +0800

    Add create table api

---
+++ src/aws_dynamo_v2_create_table.c
@@ -0,0 +1,31 @@
+/*
+ * Copyright (c) 2012-2014 Devicescape Software, Inc.
+ * This file is part of aws_dynamo, a C library for AWS DynamoDB.
+ *
+ * aws_dynamo is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * aws_dynamo is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with aws_dynamo.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#define _GNU_SOURCE
+
+#include <stdlib.h>
+
+#include "http.h"
+#include "aws_dynamo.h"
+#include "aws_dynamo_v2_create_table.h"
+
+int aws_dynamo_v2_create_table(struct aws_handle *aws, const char *request)
+{
+    return aws_dynamo_request(aws, AWS_DYNAMO_V2_CREATE_TABLE, request);
+}
+
